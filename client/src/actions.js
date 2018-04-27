a24809dbd220f5ba781ac6a1833eb44fdebb70f4
purepiko <meme_chairr@hotmail.com>
2018-04-27 15:15:21 +0700

    seperate actions

---
+++ client/src/actions.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+export function fetchBear(){
+
+    return (dispatch) => {
+
+      axios.get('http://localhost:8000/api/bears')
+      .then(result => {
+        dispatch({type: 'FETCH_BEAR', payload: result.data})
+        
+      })
+    }
+  }
